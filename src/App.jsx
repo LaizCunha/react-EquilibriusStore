89aef7275c7865439eabcb50f050cd4f5db92d8a
Laiz <laizsc.89@gmail.com>
2024-03-26 16:12:48 -0300

    Criando rotas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Products from './pages/Products';
+import Details from './pages/Details';
+import Checkout from './pages/Checkout';
 
 function App() {
 
 
   return (
-    <>
-      <Login></Login>
+    <>      
+      <Routes>
+        <Route exact path="/" element={ <Login /> } />
+        <Route path="/products" element={ <Products /> } />
+        <Route path="/details" element={ <Details />} />
+        <Route path="/checkout" element={ <Checkout /> } />
+      </Routes>
     </>
   )
 }
