eda9762c89e5301631e7d6a5b0f581d9100dd93d
Laiz <laizsc.89@gmail.com>
2024-04-01 11:00:07 -0300

    Implementa o component Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,20 @@
+import "./Footer.css"
+
 function Footer() {
     return(
         <>
+            <div className="footer">
+                <div>
+                    Fale Conosco: <br/>
+                    (071) 99999-0000 <br/>
+                    faleconosco@equilibrius.com.br
+                </div>
+                <div>
+                    Endereço: <br/>
+                    Rua SalvadorTech - Salvador - BA <br/>
+                    CEP - 400.000-100
+                </div>
+            </div>
         </>
     )
 }
