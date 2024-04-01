a814832b2ac584fc5a890110a333e338d91138e3
Laiz <laizsc.89@gmail.com>
2024-04-01 10:47:19 -0300

    Implementa o component Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+import "./Header.css"
+import icon from "../assets/icon.png"
+
 function Header() {
     return (
-        <>
-        </>
+      <>
+        <nav className="navbar">
+          <div className="container">
+            <img className="logo" src={icon}></img>
+              <ul className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+                <Link to="/checkout">Checkout</Link>
+              </ul>
+          </div>
+        </nav>      
+      </>
     )
 }
 
