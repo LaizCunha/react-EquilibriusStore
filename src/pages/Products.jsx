2d2cbc5cddf1b4885846f83dec11b2228e17048f
Laiz <laizsc.89@gmail.com>
2024-04-05 10:21:34 -0300

    Cria a page Products:
    *Renderiza os produtos no site;
    *Adiciona lógica do carrinho na página principal.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,23 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import SmallCardList from "../components/SmallCardList";
+import Cart from "../components/Cart";
+import { useState } from "react";
+
 function Products() {
+
+  const [itensCarrinho, setItensCarrinho] = useState([]);
+
+
     return(
-        <>
-          <p>Teste</p>
-        </>
+      <>
+        <Header></Header>
+        <Cart itensCarrinho={itensCarrinho} setItensCarrinho={setItensCarrinho}></Cart>
+        <br />
+        <br />
+        <SmallCardList itensCarrinho={itensCarrinho} setItensCarrinho={setItensCarrinho}></SmallCardList>
+        <Footer></Footer>
+      </>
     )
 }
 
