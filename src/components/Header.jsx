3778fd433ad755c8088e4a15d0147c56115f65ef
Laiz <laizsc.89@gmail.com>
2024-04-22 15:26:39 -0300

    Editando NavBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,15 +6,21 @@ function Header() {
     return (
       <>
         <nav className="navbar">
-          <div className="container">
-            <img className="logo" src={icon}></img>
-              <ul className="nav-links">
-                <Link to="/">Home</Link>
-                <Link to="/products">Products</Link>
-                <Link to="/checkout">Checkout</Link>
-              </ul>
-          </div>
-        </nav>      
+          <ul className="list">
+            <li className="item">
+              <Link to="/"><img src={icon} alt="Logo" /></Link>
+            </li>
+            <li className="item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="item">
+            <Link to="/products">Products</Link>
+            </li>
+            <li className="item"> 
+            <Link to="/checkout">Checkout</Link>
+            </li>
+        </ul>
+    </nav> 
       </>
     )
 }
