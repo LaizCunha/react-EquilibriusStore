4abf458e2e11ecf47225004cc9299d5bd318a20a
Laiz <laizsc.89@gmail.com>
2024-04-22 15:27:19 -0300

    Criando a page Checkout.

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from 'react';
+import Cart from '../components/Cart';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import '../components/CartCheckout.css';
+
 function Checkout() {
+
+  const [itensCarrinho, setItensCarrinho] = useState([]);
+  const isCheckout = true;
+
+  useEffect(() => {
+    const storedCart = JSON.parse(localStorage.getItem('itensCarrinho'));
+    if(storedCart) {
+      setItensCarrinho(storedCart);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('itensCarrinho', JSON.stringify(itensCarrinho));
+  }, [itensCarrinho]);
+
+  useEffect(() => {
+    if (itensCarrinho.length > 0) {
+      localStorage.setItem('itensCarrinho', JSON.stringify(itensCarrinho));
+    } else {
+      localStorage.removeItem('itensCarrinho');
+    }
+  }, [itensCarrinho]);
+
     return (
         <>
-          <p>Teste</p>
+          <Header></Header>
+          <Cart itensCarrinho={itensCarrinho} setItensCarrinho={setItensCarrinho} isCheckout={isCheckout}></Cart>
+          <Footer></Footer>
         </>
     )
 }
