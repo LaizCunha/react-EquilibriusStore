6b2c1aa991b4d5a167b07fcfd929c86646dcd57d
Laiz <laizsc.89@gmail.com>
2024-04-25 11:39:30 -0300

    Cria o component de Loading e implementa na page Products.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,23 +2,43 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import SmallCardList from "../components/SmallCardList";
 import Cart from "../components/Cart";
-import { useState } from "react";
+import Loading from "../components/layout/Loading";
+import './Products.css';
+
+import { useState, useEffect } from "react";
 
 function Products() {
 
   const [itensCarrinho, setItensCarrinho] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
 
-    return(
-      <>
-        <Header></Header>
-        <Cart itensCarrinho={itensCarrinho} setItensCarrinho={setItensCarrinho}></Cart>
-        <br />
-        <br />
-        <SmallCardList itensCarrinho={itensCarrinho} setItensCarrinho={setItensCarrinho}></SmallCardList>
+    const storedCart = JSON.parse(localStorage.getItem('itensCarrinho'));
+    setTimeout(() => {
+      if (storedCart) {
+        setItensCarrinho(storedCart);
+      }
+      setLoading(false)
+    }, 500);
+  }, []);
+
+  return(
+    <>
+      <Header />
+      <div className="products-container">
+        {loading ? (<Loading/>) : (
+          <>
+            <Cart itensCarrinho={itensCarrinho} setItensCarrinho={setItensCarrinho}></Cart>
+            <br />
+            <br />
+            <SmallCardList itensCarrinho={itensCarrinho} setItensCarrinho={setItensCarrinho}></SmallCardList>
+          </> 
+        )}
         <Footer></Footer>
-      </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default Products;
